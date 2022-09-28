9fc0ce51907d93aae2a4a39b5e8f7d4bab6e0d59
jbarzallo <arielbarzallo-97@hotmail.com>
2022-09-27 22:48:04 -0500

    proyecto cambio de fondo

---
+++ app.js
@@ -0,0 +1,14 @@
+boton.addEventListener('click', function(){
+    let color = aleatorio()
+    body.style.backgroundColor  =  color;
+    b.style.color  =  color;
+    document.getElementById("b").innerHTML = color;
+})
+
+function aleatorio (){
+    let n1= (Math.round(((Math.random()).toFixed(6)) * 255))
+    let n2= (Math.round(((Math.random()).toFixed(6)) * 255))
+    let n3= (Math.round(((Math.random()).toFixed(6)) * 255))
+
+    return `rgb(${n1}, ${n2}, ${n3})`;
+}
